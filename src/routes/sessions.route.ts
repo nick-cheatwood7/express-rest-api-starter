ef00ccda9ef55213217fece31c9a2b05048d0933
nick-cheatwood7 <nick.cheatwood@gmail.com>
2022-07-27 13:13:12 -0500

    feat: added docs for Session routes

--- src/routes/sessions.route.ts
+++ src/routes/sessions.route.ts
@@ -10,8 +10,94 @@ import { createSessionSchema } from "../schema/session.schema";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/sessions:
+ *  post:
+ *    tags:
+ *    - Sessions
+ *    summary: Create a Session (log in)
+ *    requestBody:
+ *      required: true
+ *      content:
+ *          application/json:
+ *             schema:
+ *              $ref: '#/components/schemas/CreateSessionInput'
+ *    responses:
+ *      201:
+ *          description: Created
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          accessToken:
+ *                              type: string
+ *                          refreshToken:
+ *                              type: string
+ *      401:
+ *          description: Invalid credentials
+ *      400:
+ *          description: Bad Request
+ */
 router.post("/", validateResource(createSessionSchema), createUserSessionHandler);
+
+/**
+ * @swagger
+ * /api/sessions:
+ *  get:
+ *    tags:
+ *    - Sessions
+ *    summary: Get all Sessions for the current User
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                  $ref: '#/components/schemas/Session'
+ *      400:
+ *          description: Bad request
+ *      403:
+ *          description: Forbidden
+ */
 router.get("/", requireUser, getUserSessionHandler);
+
+/**
+ * @swagger
+ * /api/sessions/{sessionId}:
+ *  delete:
+ *    tags:
+ *    - Sessions
+ *    summary: Delete a Session (logout)
+ *    parameters:
+ *    - name: sessionId
+ *      in: path
+ *      description: The id of the Session
+ *      required: true
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *               schema:
+ *                  type: object
+ *                  properties:
+ *                      accessToken:
+ *                          type: string
+ *                          nullable: true
+ *                          default: null
+ *                      refreshToken:
+ *                          type: string
+ *                          nullable: true
+ *                          default: null
+ *      400:
+ *          description: Bad Request
+ *      403:
+ *          description: Forbidden
+ */
 router.delete("/", requireUser, deleteSessionHandler);
 
 export default router;
--- src/schema/session.schema.ts
+++ src/schema/session.schema.ts
@@ -1,5 +1,38 @@
 import { z } from "zod";
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      CreateSessionInput:
+ *          type: object
+ *          required:
+ *              - email
+ *              - password
+ *          properties:
+ *              email:
+ *                  type: string
+ *                  example: jane_doe@example.com
+ *              password:
+ *                  type: string
+ *      Session:
+ *          type: object
+ *          properties:
+ *              id:
+ *                  type: string
+ *              createdAt:
+ *                  type: string
+ *                  format: date-time
+ *              updatedAt:
+ *                  type: string
+ *                  format: date-time
+ *              userId:
+ *                  type: string
+ *              valid:
+ *                  type: boolean
+ *              userAgent:
+ *                  type: string
+ */
 export const createSessionSchema = z.object({
     body: z.object({
         email: z.string({ required_error: "Email is required" }).email("Invalid email"),
